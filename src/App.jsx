32f3855c9d2b2cf71263cd85c554d261efdebe69
Mati Joseph <crucialniccur@gmail.com>
2025-04-11 11:37:13 +0300

    add ana array of fav snacks to render

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,48 @@ import Homepage from "./Homepage";
 import PetCard from "./PetCard";
 
 const pets = [
-  { id: 1, name: "Fezzik", image: "https://picsum.photos/200" },
-  { id: 2, name: "Lucy", image: "https://picsum.photos/200/?blur" },
-  { id: 3, name: "Inda", image: "https://picsum.photos/200/?grayscale" },
-  { id: 4, name: "Moha", image: "https://picsum.photos/200/?blur" },
-  { id: 5, name: "Liza", image: "https://picsum.photos/200/?" },
+  {
+    id: 1,
+    name: "Fezzik",
+    image: "https://picsum.photos/200",
+    favSnacks: ["Peanut Butter", "Soccer Balls"],
+  },
+  {
+    id: 2,
+    name: "Lucy",
+    image: "https://picsum.photos/200/?blur",
+    favSnacks: ["mayai", "chapti innit"],
+  },
+  {
+    id: 3,
+    name: "Inda",
+    image: "https://picsum.photos/200/?grayscale",
+    favSnacks: ["Cake", "Bacon"],
+  },
+  {
+    id: 4,
+    name: "Moha",
+    image: "https://picsum.photos/200/?blur",
+    favSnacks: ["matai", "anjera", "nyama"],
+  },
+  {
+    id: 5,
+    name: "Liza",
+    image: "https://picsum.photos/200/?",
+    favSnacks: ["Pigeons", "Grass", "Chicken"],
+  },
 ];
 
 function App() {
   const petCards = pets.map((petObj) => {
-    return <PetCard key={petObj.id} name={petObj.name} src={petObj.image} />;
+    return (
+      <PetCard
+        key={petObj.id}
+        name={petObj.name}
+        src={petObj.image}
+        favSnacks={petObj.favSnacks}
+      />
+    );
   });
 
   console.log(petCards);
--- src/PetCard.jsx
+++ src/PetCard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 function PetCard(props) {
+  //   console.log(props);
+
+  console.log(props.favSnacks);
+  const showSnacks = props.favSnacks.map((snack) => <ul>{snack}</ul>);
+
   return (
     <div className="card">
       <h2>{props.name}</h2>
       <img src={props.src} alt={props.name} />
+      <h3>Favorite snacks innit</h3>
+      {showSnacks}
+      {/* <ul>{props.favSnacks}</ul> */}
     </div>
   );
 }
