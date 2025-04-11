3388061de8f0a3d260f8d02f9d673d67f2abb98a
Mati Joseph <crucialniccur@gmail.com>
2025-04-11 05:06:32 +0300

    copy the code to the card

--- src/Createcard.jsx
+++ src/Createcard.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const CreateCard = () => {
-  return <div>createCard</div>;
+  return (
+    <div>
+      <header>
+        <h1>Pet Land</h1>
+        <nav>
+          <button>
+            <span role="img">👤</span>
+            Loginn
+          </button>
+        </nav>
+      </header>
+      <main>
+        <div className="card">
+          <h2>Welcome to Petland</h2>
+          <em>Find dream pet</em>
+        </div>
+        <div className="card">
+          <h2>WHat pets would you like to see?</h2>
+          <div>
+            <button>
+              <span role="img">😸</span>
+              Cats
+            </button>
+            <button>
+              <span role="img">🐕</span>
+              Dogs
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default CreateCard;
