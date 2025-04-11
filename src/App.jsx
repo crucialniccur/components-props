03015bcf398f2a63e67cca7edc132d279516aec9
Mati Joseph <crucialniccur@gmail.com>
2025-04-11 05:53:37 +0300

    remove un necessary card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 import "./App.css";
-import CreateCard from "./Createcard";
+import React from "react";
+import EmojiBUtton from "./EmojiBUtton";
+import Header from "./Header";
+import Card from "./Card";
+import Homepage from "./Homepage";
+
+const pets = [
+  { id: 1, name: "Fezzik", image: "https://picsum.photos/200/300" },
+  { id: 2, name: "Lucy", image: "https://picsum.photos/200/300" },
+];
 
 function App() {
-  return <CreateCard />;
+  return (
+    <div>
+      <Header heading="Pet Landia" />
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
